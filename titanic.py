b6e32fa642beb8df6252dcb5e3f6d7121f41276c
vasanthaaaaa <143865911+vasanthaaaaa@users.noreply.github.com>
2024-01-12 10:44:43 +0530

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('titanic')
 st.write('titanic')
-st.image('https://akm-img-a-in.tosshub.com/indiatoday/titanic_647_041416113640.jpg?size=690:388'),content='titanic'
+st.image('https://akm-img-a-in.tosshub.com/indiatoday/titanic_647_041416113640.jpg?size=690:388',caption='titanic')
