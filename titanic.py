7e19af48fa9b5ebb73e92b3eea6bd480d50fdb6a
vasanthaaaaa <143865911+vasanthaaaaa@users.noreply.github.com>
2024-01-12 10:33:12 +0530

    Create titanic.py

---
+++ titanic.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('titanic')
+st.write('titanic')
+st.image('https://akm-img-a-in.tosshub.com/indiatoday/titanic_647_041416113640.jpg?size=690:388'),content='titanic'
